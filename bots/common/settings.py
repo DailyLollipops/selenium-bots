c2dfa14dca50663cf0037e2ce61b6fcaef68327d
DailyLollipops <clarencemadrigal84@gmail.com>
2024-08-16 01:52:29 +0800

    Fix provision, removed individual service vnc port

--- bots/common/settings.py
+++ bots/common/settings.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()
 
 class Settings(BaseModel):
-    HUB_URL: str = os.getenv('HUB_URL') or 'http://selenium-hub:4444/wd/hub'
+    HUB_PORT: int = os.getenv('HUB_PORT') or 4444
+    HUB_URL: str = f'http://selenium-hub:{HUB_PORT}/wd/hub'
     LOG_DIR: str = os.getenv('LOG_DIR') or '/usr/src/selenium-bots/logs'
     PROXYMESH_USERNAME: str = os.getenv('PROXYMESH_USERNAME') or ''
     PROXYMESH_PASSWORD: str = os.getenv('PROXYMESH_PASSWORD') or ''
