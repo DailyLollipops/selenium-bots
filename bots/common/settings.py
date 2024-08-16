a8397406abba1b8bb267612bf78184791f550369
DailyLollipops <clarencemadrigal84@gmail.com>
2024-08-17 02:31:17 +0800

    Moved log folder to temp

--- bots/common/settings.py
+++ bots/common/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Settings(BaseModel):
     HUB_PORT: int = os.getenv('HUB_PORT') or 4444
     HUB_URL: str = f'http://selenium-hub:{HUB_PORT}/wd/hub'
-    LOG_DIR: str = os.getenv('LOG_DIR') or '/usr/src/selenium-bots/logs'
+    LOG_DIR: str = os.getenv('LOG_DIR') or 'temp/logs'
     DOWNLOAD_DIR: str = os.getenv('DOWNLOAD_DIR') or 'temp/downloads'
     PROXYMESH_USERNAME: str = os.getenv('PROXYMESH_USERNAME') or ''
     PROXYMESH_PASSWORD: str = os.getenv('PROXYMESH_PASSWORD') or ''
